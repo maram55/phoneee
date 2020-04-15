20d1c5a24f4c8449789c7f189092915bd290179f
maram55 <maramnashat5@gmail.com>
2020-04-15 11:27:41 +0200

    intial commit

---
+++ Phone/Controllers/CategoriesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Phone.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // GET: Categories
+        public ActionResult Categories()
+        {
+            return View();
+        }
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        public ActionResult DeleteCategory()
+        {
+            return View();
+        }
+        public ActionResult Details()
+        {
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
---
+++ Phone/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Phone.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Products()
+        {
+            return View();
+        }
+        public ActionResult AddProducts()
+        {
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+}
